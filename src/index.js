b50596c72b5a30389698c73a64e70d0101ddd654
Chetan Singh <97108649+chetansingh03@users.noreply.github.com>
2022-07-22 01:22:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router } from ‘react-router-dom’;
+
 import Nav from "./nav";
 import Nv, { Corousel, Off, Sale } from "./navv";
 import Brands, { Footer } from "./brands";
@@ -11,9 +11,9 @@ import A from "./navlink";
 
 ReactDOM.render(
   <>
-  <Router basename={process.env.PUBLIC_URL}>
+
     <A />
-  </Router>
+ 
   </>,
   document.getElementById("new")
 );
